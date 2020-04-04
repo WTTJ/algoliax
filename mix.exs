b1c865bc1006f6008483aa4d5b1f712b55575adb
StephaneRob <stephane.robino@gmail.com>
2020-04-04 21:38:03 +0200

    realease v0.3.0
    Signed-off-by: Stéphane Robino <stephane@wttj.co>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Algoliax.MixProject do
   def project do
     [
       app: :algoliax,
-      version: "0.3.0-beta",
+      version: "0.3.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
