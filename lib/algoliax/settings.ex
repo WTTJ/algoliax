b9870f718c24f3d489f6276b73300cbf32def639
Jordan Kowal <kowaljordan@gmail.com>
2025-02-26 11:28:04 +0100

    fix: update supported settings list

--- lib/algoliax/settings.ex
+++ lib/algoliax/settings.ex
@@ -3,23 +3,32 @@ defmodule Algoliax.Settings do
 
   import Algoliax.Utils, only: [camelize: 1, algolia_settings: 2]
 
+  # https://www.algolia.com/doc/api-reference/settings-api-parameters/
   @algolia_settings [
+    # Attributes
     :searchable_attributes,
     :attributes_for_faceting,
     :unretrievable_attributes,
     :attributes_to_retrieve,
+    # Ranking
+    :mode,
     :ranking,
     :custom_ranking,
+    :relevancy_strictness,
+    # Faceting
     :max_values_per_facet,
     :sort_facet_values_by,
+    # Highlighting/Snippeting
     :attributes_to_highlight,
     :attributes_to_snippet,
     :highlight_pre_tag,
     :highlight_post_tag,
     :snippet_ellipsis_text,
     :restrict_highlight_and_snippet_arrays,
+    # Pagination
     :hits_per_page,
     :pagination_limited_to,
+    # Typos
     :min_word_sizefor1_typo,
     :min_word_sizefor2_typos,
     :typo_tolerance,
@@ -27,13 +36,22 @@ defmodule Algoliax.Settings do
     :disable_typo_tolerance_on_attributes,
     :disable_typo_tolerance_on_words,
     :separators_to_index,
+    # Languages
     :ignore_plurals,
+    :attributes_to_transliterate,
     :remove_stop_words,
     :camel_case_attributes,
     :decompounded_attributes,
     :keep_diacritics_on_characters,
+    :custom_normalization,
     :query_languages,
+    :index_languages,
+    :decompound_query,
+    # Rules
     :enable_rules,
+    # Personalization
+    :enable_personalization,
+    # Query strategy
     :query_type,
     :remove_words_if_no_results,
     :advanced_syntax,
@@ -42,18 +60,20 @@ defmodule Algoliax.Settings do
     :disable_exact_on_attributes,
     :exact_on_single_word_query,
     :alternatives_as_exact,
+    :advanced_syntax_features,
+    # Performance
     :numeric_attributes_for_filtering,
     :allow_compression_of_integer_array,
-    :numeric_attributes_to_index,
+    # Advanced
     :attribute_for_distinct,
     :distinct,
     :replace_synonyms_in_highlight,
     :min_proximity,
     :response_fields,
     :max_facet_hits,
-    :synonyms,
-    :placeholders,
-    :alt_corrections
+    :attribute_criteria_computed_by_min_proximity,
+    :user_data,
+    :rendering_content,
   ]
 
   def settings do
